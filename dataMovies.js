d87d3ddd74cc3f416b45e97e2fec0de5e0e3bfd8
Eduardo_Morales <eduardo@inmo-puebla.com>
2022-02-03 21:08:20 -0600

    creando repositorio

---
+++ dataMovies.js
@@ -0,0 +1,37 @@
+let movies = [
+  {
+    name: "falcon and the winter Soldier",
+    description:
+      "Falcon y Winter Soldier, Falcon y el Soldado de Invierno en Latinoamerica, sigue la historia de Sam Wilson y Bucky Barnes tras los acontecimientos de Vengadores: Endgame, donde deben lidiar con el legado de poder llegar a ser el nuevo Capitán América , Steve Rogers ha dejado atrás su papel de superhéroe tras entregar las gemas del infinito a su correspondiente línea temporal, otorgando así su escudo a Falcon. Sin embargo, éste no está muy de acuerdo de seguir su legado porque cree que solo hay un Capitán América y es él Aunque su relación nunca han sido de mejores amigos, Falcon y el Soldado de Invierno deben luchar juntos contra una nueva amenaza, mientras también deben lidiar con sus problemas personales, como es el caso de Bucky y sus pesadillas por su pasado",
+
+    image: "./images//slider 2.png",
+  },
+  {
+    name: "loki",
+    description:
+      "Loki es una serie que gira en torno al Dios del Engaño de Marvel y que retoma su historia justo después de cuando éste se hace con el Teseracto en Vengadores: Endgame, la última película de la Fase 3 del Universo Cinematográfico de Marvel.Tras este acontecimiento, Loki se encuentra con la Agencia de Variación Temporal (AVT), una organización burocrática que existe fuera del tiempo y el espacio, obligado a responder por sus crímenes que realizó contra la línea del tiempo y le da una opción: Ser eliminado de la realidad o ayudar a atrapar una amenaza aún mayor.En esta nueva organización del universo Marvel, Loki conoce a Mobius, un agente de la AVT que le salva de su destino cuando le da la oportunidad de unirse a él para dar caza a una persona que intenta acabar con la Sagrada Línea Temporal, no sin antes mostrarle el futuro que tenía el Loki original y que éste desconoce totalmente.",
+
+    image: "./images/slider 1.png",
+  },
+  {
+    name: "Wanda Vision",
+    description:
+      "WandaVision, Bruja Escarlata y Visión en España, sigue la historia de Bruja Escarlata. En un principio, la vimos como una supervillana miembro de la Hermandad de Mutantes. Esta superheroína, que se convirtió en miembro de Los Vengadores, posee poderes para cambiar la realidad de varias formas no específicas además de ser una gran hechicera.La serie sigue en concreto la historia de Bruja Escarlata y Visión que se encuentran dentro de una constante sitcom, desde la edad de oro de la televisión hasta hoy día. Pasando asi por todos los elementos de comedia de una serie de este tipo mientras se intenta aclarar que es lo que le ha pasado a Wanda Maximoff y a Visión tras los acontecimientos de Vengadores: Endgame.",
+
+    image: "./images/slider 3.png",
+  },
+  {
+    name: "raya",
+    description:
+      "Ambientada en el sudeste asiático, la cinta sigue la historia de Raya, una niña de un gran espíritu aventurero que vive en un reino llamado Kumandra. Este lejano y recóndito territorio está habitado por una civilización milenaria. Cinco son los clanes que forman esta tierra conocida como la del dragón y Raya, con el objetivo de devolver el equilibrio a su tierra, será la encargada de encontrar al último de los dragones en la mayor aventura de su vida.",
+
+    image: "./images/slider 4.png",
+  },
+  {
+    name: "luka",
+    description:
+      "Ambientada en un precioso pueblo italiano costero (está claramente inspirada en los pueblos de la zona conocida como Cinque Terre), esta historia de convivencia entre lugareños y monstruos marinos parece una metáfora sobre la inmigración. Quizás en algunos aspectos es demasiado amable, pero está claro que estamos hablando de una película de animación dirigida a los pequeños y las pequeñas y ello hace que el mensaje siempre se simplifique. En cuanto a la animación, no es de las más destacadas de la compañía ni mucho menos, es más bien simple pero efectista, sobre todo en el colorido, pero la aventura es bastante entretenida.",
+
+    image: "./images//slider 1.png",
+  },
+];
